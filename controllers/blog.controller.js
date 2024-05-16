c3ee44a2397b5daaa8ca8f0fa694e1435fa4e2b9
sameerse <sameer2121065@iimscollege.edu.np>
2024-05-16 13:18:35 +0545

    bug fixes

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,9 +1,9 @@
-const blog = require("../models/blog.model");
+const Blog = require("../models/blog.model");
 
 //   api request to get all blogs
 const getBlogs = async (req, res) => {
   try {
-    const blogs = await blog.find();
+    const blogs = await Blog.find();
     res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -13,8 +13,8 @@ const getBlogs = async (req, res) => {
 // api request to get a blog by id
 const getBlog = async (req, res) => {
   try {
-    const blog = await blog.findById(req.params.id);
-    res.json(blog);
+    const blog = await Blog.findById(req.params.id);
+    res.status(200).json(blog);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -22,27 +22,27 @@ const getBlog = async (req, res) => {
 
 // api request to create a blog
 const createBlog = async (req, res) => {
-  const newBlog = new blog({
+  const newBlog = new Blog({
     title: req.body.title,
     category: req.body.category,
     description: req.body.description,
   });
   try {
     const savedBlog = await newBlog.save();
-    res.json(savedBlog);
+    res.status(200).json(savedBlog);
   } catch (error) {
-    res.status.json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 // api request to update a blog
 const updateBlog = async (req, res) => {
   try {
-    const updatedBlog = await blog.updateOne(
+    const updatedBlog = await Blog.updateOne(
       { _id: req.params.id },
       { $set: { title: req.body.title } }
     );
-    res.json(updatedBlog);
+    res.status(200).json(updatedBlog);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, function () {
+    app.listen(3000, () => {
       console.log("Server is running on port 3000");
     });
   })
